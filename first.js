1efd2987a2e217a6e0ed467cd2fa24a7411cc681
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-03-30 23:54:00 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+//Theory portion of javascript
+
+// 1. javascript is case sensitive.
+// 2.it is dynamically type program that means if you declare let a=5 and then you want to change that a="Rajeev" you can easily change at runtime.
+// 3.letter, underscore & $ sign are allowed.
+// 4. Before ES6(ecmascript), in javascript for declaring variable we were used Var.
+// 5.const is used to declare constant. 
+// 6. const can neither updated and nor re-declared. 
+// 7. var can be updated & re-declared within its scope.
+// 8. let can be updated & but not re-declared.
+// 9.var is globally scoped while let and const are blocked scoped.
+//10. Data type means what type of value store in that variable.
+//11.There are 7 types of data type.
+
+let a="rajeev";
+let b=11;
+console.log(a+b);
+console.log(typeof (a+b));
